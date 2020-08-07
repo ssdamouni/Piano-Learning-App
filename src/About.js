0238e7201528b5ca0ac2b9131f6d3c4fd87d8a79
ssdamouni <66917932+ssdamouni@users.noreply.github.com>
2020-08-07 13:52:58 -0700

    Updated About page

--- src/About.js
+++ src/About.js
@@ -4,7 +4,8 @@ import './About.css';
 function About(){
     return(
         <div className="About">
-            <header>This is about us.</header>
+            <header className="About-header"> About Us</header>
+            <p className="About-p">This is a new online learning system for learning about Classical music. We offer private lessons, group music history classes, and instructional do-it-yourself videos. All you need to do is create an account to have accesss to all our wonderful products.</p>
         </div>
     )
 }

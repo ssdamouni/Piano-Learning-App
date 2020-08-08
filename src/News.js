4eccd54a40a4d5bc044930dcdca2e88d025b28c2
ssdamouni <66917932+ssdamouni@users.noreply.github.com>
2020-08-07 19:35:21 -0700

    added fields to news

--- src/News.js
+++ src/News.js
@@ -4,7 +4,8 @@ import './News.css';
 function News(){
     return(
         <div className="News">
-            <header>This is News</header>
+            <header className="News-header">New Developments</header>
+            <p className="News-p">This fall serious development will take place to roll out this website by the beginnign of 2021! This will include all front-end and back-end developement to help the students learn as efficiently as possible.</p>
         </div>
     )
 }
